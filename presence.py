6ad101dc8be0469a4cf90290e3c5604054ba61d1
Animenosekai <40539549+Animenosekai@users.noreply.github.com>
2022-01-10 15:52:25 +0100

    [update] removing Nasse from the dependencies

--- presence.py
+++ presence.py
@@ -10,36 +10,31 @@ from importlib import reload
 from random import random
 from time import sleep, time
 
-from nasse.config import General
-from nasse.logging import log
-from nasse.utils.logging import LogLevels
 from pypresence import Presence
 from requests import get
 
 import config
 
-General.NAME = "Genshin Presence"
-
 START = time()
 
 DISCORD_APPLICATION_ID = 929496938020208690  # Put your Discord Application ID
 
-log("Connecting to Discord RPC with Application ID {}".format(DISCORD_APPLICATION_ID), level=LogLevels.INFO)
+print("Connecting to Discord RPC with Application ID {}".format(DISCORD_APPLICATION_ID))
 RPC = Presence(str(DISCORD_APPLICATION_ID))
 RPC.connect()
 
 while True:
-    log("Loading current settings")
+    print("Loading current settings")
     config = reload(config)
-    log("Asking for the game data for UID {}".format(config.Player.HOYOLAB_UID), level=LogLevels.INFO)
+    print("Asking for the game data for UID {}".format(config.Player.HOYOLAB_UID))
     user_data = get("https://bbs-api-os.hoyolab.com/game_record/card/wapi/getGameRecordCard?uid={}".format(config.Player.HOYOLAB_UID),
                     headers={"Cookie": config.Settings.COOKIE}).json()
     for game_data in user_data["data"]["list"]:
         if config.Player.SERVER_REGION is not None and game_data["region"] != config.Player.SERVER_REGION.code:
             continue
         if game_data["game_id"] == config.Player.GAME.code:
-            log("Found {} data for {}".format(config.Player.GAME, game_data['region_name']))
-            log("Updating the RPC Client", level=LogLevels.INFO)
+            print("Found {} data for {}".format(config.Player.GAME, game_data['region_name']))
+            print("Updating the RPC Client")
             RPC.update(
                 state="{} 〜 Playing on {}".format(game_data['nickname'], game_data['region_name']),
                 details="Currently at AR {}".format(game_data['level']),
@@ -51,5 +46,5 @@ while True:
                 start=START
             )
             break
-    log("Waiting for the next update ({} seconds)".format(config.Settings.REFRESH_RATE), LogLevels.INFO)
+    print("Waiting for the next update ({} seconds)".format(config.Settings.REFRESH_RATE))
     sleep(config.Settings.REFRESH_RATE + random())  # random is used to humanize a little bit the refresh rate
